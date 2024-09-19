aab349bb53dbf0a611dd84c8cd2bc8ff3a9a02f5
juliancramos21 <julianc.ramos@javeriana.edu.co>
2024-09-19 20:45:39 +0000

    Realizado hasta To Do #6

--- song.cxx
+++ song.cxx
@@ -2,29 +2,90 @@
 
 // Constructor
 // Constructor
-Song::Song(int id, std::string name, std::string genre,
+Song::Song(std::string id, std::string name, std::string genre,
            std::string artists, std::string album,
            int popularity, int durationMs, bool explicitContent)
     : id(id), name(name), genre(genre), artists(artists),
       album(album), popularity(popularity),
       durationMs(durationMs), explicitContent(explicitContent) {}
 
+
 // Getters
-// TODO #4: Implementar las funciones getters de cada uno de los campos que definen una cancion. (Total de getters: 8)
+std::string Song::getId() const {
+    return id;
+}
+
+std::string Song::getName() const {
+    return nombre;
+}
+
+std::string Song::getGenre() const {
+    return genre;
+}
+
+std::string Song::getArtists() const {
+    return artists;
+}
+
+std::string Song::getAlbum() const {
+    return album;
+}
+
+int Song::getPopularity() const {
+    return popularity;
+}
+
+int Song::getDurationMs() const {
+    return durationMs;
+}
+
+bool Song::isExplicitContent() const {
+    return explicitContent;
+}
 
 // Setters
-// TODO #5: Implementar las funciones setters de cada uno de los campos que definen una cancion. (Total de setters: 8)
+void Song::setId(const std::string& newId) {
+    id = newId;
+}
+
+void Song::setName(const std::string& newNombre) {
+    nombre = newNombre;
+}
+
+void Song::setGenre(const std::string& newGenre) {
+    genre = newGenre;
+}
+
+void Song::setArtists(const std::string& newArtists) {
+    artists = newArtists;
+}
+
+void Song::setAlbum(const std::string& newAlbum) {
+    album = newAlbum;
+}
+
+void Song::setPopularity(int newPopularity) {
+    popularity = newPopularity;
+}
+
+void Song::setDurationMs(int newDurationMs) {
+    durationMs = newDurationMs;
+}
+
+void Song::setExplicitContent(bool newExplicitContent) {
+    explicitContent = newExplicitContent;
+}
+
 
 // Overloaded operators
-// TODO #6: Implementar las funciones de sobrecarga de operadores >, < e ==.
 bool Song::operator>(const Song& other) const {
-    return true;
+    return this->getId() > other.getId();
 }
 
 bool Song::operator<(const Song& other) const {
-    return true;
+    return this->getId() < other.getId();
 }
 
 bool Song::operator==(const Song& other) const {
-    return true;
+    return this->getId() == other.getId();
 }
--- song.h
+++ song.h
@@ -4,24 +4,48 @@
 #include <string>
 
 class Song {
-public:
-    // Constructor
-    Song(int id, std::string name, std::string genre,
-         std::string artists, std::string album,
-         int popularity, int durationMs, bool explicitContent);
 
-    // Getters
-    // TODO #2: Definir las funciones getters de cada uno de los campos que definen una cancion. (Total de getters: 8)
-    // Setters
-    // TODO #3: Definir las funciones setters de cada uno de los campos que definen una cancion. (Total de setters: 8)
+    private:
+    std::string id;
+    std::string name;
+    std::string genre;
+    std::string artists;
+    std::string album;
+    int popularity;
+    int durationMs;
+    bool explicitContent; //Identifica si la canción contiene contenido explicito
 
-    // Overloaded operators
-    bool operator>(const Song& other) const;
-    bool operator<(const Song& other) const;
-    bool operator==(const Song& other) const;
+    public:
+        // Constructor
+        Song(std::string id, std::string name, std::string genre,
+            std::string artists, std::string album,
+            int popularity, int durationMs, bool explicitContent);
 
-private:
-    // TODO #1: Definir cada uno de los campos de la cancion. (Total de campos: 8)
+        // Getters
+        std::string getId() const;
+        std::string getName() const;
+        std::string getGenre() const;
+        std::string getArtists() const;
+        std::string getAlbum() const;
+        int getPopularity() const;
+        int getDurationMs() const;
+        bool isExplicitContent() const;
+
+        // Setters
+        void setId(const std::string& id);
+        void setName(const std::string& nombre);
+        void setGenre(const std::string& genre);
+        void setArtists(const std::string& artists);
+        void setAlbum(const std::string& album);
+        void setPopularity(int popularity);
+        void setDurationMs(int durationMs);
+        void setExplicitContent(bool explicitContent);
+
+        // Overloaded operators
+        bool operator>(const Song& other) const;
+        bool operator<(const Song& other) const;
+        bool operator==(const Song& other) const;
+
 };
 
 #endif // SONG_H
